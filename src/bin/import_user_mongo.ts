5322c5598bcd544376f894fbc0b85ae7160e4345
rossz <rzhu@pegasus.net.au>
2018-02-12 16:32:12 +1100

    Add more contacts

--- src/bin/import_user_mongo.ts
+++ src/bin/import_user_mongo.ts
@@ -2,6 +2,48 @@ import {User} from '../mongoose/user';
 
 let user_data = [
     { 
+        name: 'wilsonc', 
+        password: 'password', 
+        id: 'ibc_Oere3fae7zu4r', 
+        email: 'ioikos@gmail.com',
+        access_level: 5
+    },
+    { 
+        name: 'selinal', 
+        password: 'password', 
+        id: 'ibc_Gei4sooZ9daib', 
+        email: 'yklchan@gmail.com',
+        access_level: 5
+    },
+    { 
+        name: 'jackc', 
+        password: 'password', 
+        id: 'ibc_PheemahK5ojei', 
+        email: 'jhhcheng@netvigator.com',
+        access_level: 1
+    },
+    { 
+        name: 'dawny', 
+        password: 'password', 
+        id: 'ibc_am6thibaeL8eY', 
+        email: 'dawnyeye@yahoo.com.hk',
+        access_level: 1
+    },
+    { 
+        name: 'keithJ', 
+        password: 'password', 
+        id: 'ibc_Fae0ooPaijos9', 
+        email: 'keithj@unknown',
+        access_level: 1
+    }, 
+    { 
+        name: 'caroly', 
+        password: 'password', 
+        id: 'ibc_rool6ieT8Tee1', 
+        email: '1139455487q@gmail.com',
+        access_level: 1
+    },       
+    { 
         name: 'rossz', 
         password: 'password', 
         id: 'ibc_Leingie4oogit', 
