f3ea8448056f7ac14ef2e2138c03a0ef2b301ad3
rossz <rzhu@pegasus.net.au>
2018-05-21 16:40:08 +1000

    Enhance send-notification binary, such that we can use --users=user1,user2 to specify recipients.

--- src/bin/send-notifications.ts
+++ src/bin/send-notifications.ts
@@ -2,6 +2,31 @@ import {User} from '../mongoose/user';
 import {makeRandomString} from '../utils';
 import {Telstra} from '../utils/telstra';
 import {Auth} from '../utils/auth';
+const argv = require( 'argv' );
+const args = argv.option([
+{
+    name: 'confirm',
+    short: 'c',
+    type: 'boolean',
+    description: 'Confirm to send SMS',
+    example: "'script --confirm' or 'script -c'"
+},
+{
+    name: 'users',
+    short: 'u',
+    type: 'csv,string',
+    description: 'SMS recipients',
+    example: "'script --users=rossz,jillzhang' or 'script -u'"
+}
+]).run();
+
+let confirm = args.options.confirm;
+let users = args.options.users;
+
+if (!users) {
+    console.log('You must use --users=user1,user2, or -u user1,user2 to specify SMS recipients!');
+    process.exit(0);
+}
 
 import * as fs from 'fs';
 import * as async from 'async';
@@ -11,7 +36,7 @@ import {FirebaseDB as db} from '../firebase';
 let iosUrl = "https://itunes.apple.com/us/app/依斯靈頓中文教會/id1338517393?ls=1&mt=8";
 let androidUrl = "https://play.google.com/store/apps/details?id=com.rjwebsolution.ibcchinese";
 
-let usernames = ['rossz', 'jillzhang', 'wilsonc', 'selinal', 'jackc', 'dawny', 'pauliney', 'amosl', 'marcol'];
+// let usernames = ['rossz', 'jillzhang', 'wilsonc', 'selinal', 'jackc', 'dawny', 'pauliney', 'amosl', 'marcol'];
 // let usernames = ['rossz'];
 
 User.connect();
@@ -44,7 +69,7 @@ function sendNotification(names: string[], calback?: Function) {
 
             notifs = notifs.filter(n => n.mobile && n.mobile.match(/^\d+$/))
 
-            if (process.argv[2] != 'confirm') {
+            if (!confirm) {
                 console.log('--- check before sending ---');
                 console.log(notifs);
                 User.disconnect(() => {
@@ -76,4 +101,4 @@ function sendNotification(names: string[], calback?: Function) {
     })
 }
 
-sendNotification(usernames);
+sendNotification(users);
